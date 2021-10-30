1243bb1624613a3aeb843bcf49394d0f23f159b2
voidedman <92179972+voidedman@users.noreply.github.com>
2021-10-30 14:55:40 -0400

    Add files via upload

---
+++ PlayerTP.cs
@@ -0,0 +1,32 @@
+﻿#region
+
+using System.Linq;
+using System.Threading;
+using Microsoft.VisualBasic;
+using Element.ClientBase;
+
+#endregion
+
+namespace Element.Modules
+{
+    internal class PlayerTP : Module
+    {
+        public PlayerTP() : base("PlayerTP", (char)0x07, "Exploits")
+        {
+        } // Not defined
+
+        public override Element OnEnable()
+        {
+            new Thread(() =>
+            {
+                var username = Interaction.InputBox("Please enter player username", "Element (PlayerTP)").ToLower();
+
+                foreach (var entity in Game.getPlayers().Where(entity => entity.username.ToLower().Contains(username)))
+                {
+                    Game.position = entity.position;
+                    break;
+                }
+            }).Start();
+        }
+    }
+}
